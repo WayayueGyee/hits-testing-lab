b8654f55d875b30066e3afc0eaabee717172589a
vladovello <vladsuhankov@gmail.com>
2023-05-01 18:46:31 +0700

    :sparkles: feat(Alg-unit): Added unit tests for common and cross-over cases

--- src/main/kotlin/com/hits/testing/alg/MaxProfit.kt
+++ src/main/kotlin/com/hits/testing/alg/MaxProfit.kt
@@ -6,8 +6,8 @@ class MaxProfit private constructor() {
     companion object {
         private const val MIN_PRICES_LEN = 1
         private const val MAX_PRICES_LEN = 10000
-        private const val LOWER_BOUNDARY = 0
-        private const val UPPER_BOUNDARY = 1000
+        private const val PRICES_COST_LOWER_BOUNDARY = 0
+        private const val PRICES_COST_UPPER_BOUNDARY = 1000
 
         fun calculate(prices: IntArray): Result<Int> {
             if (prices.size < MIN_PRICES_LEN || prices.size > MAX_PRICES_LEN) return Result.failure(
@@ -18,7 +18,7 @@ class MaxProfit private constructor() {
             var maxDiff = 0
 
             for (i in 1 until prices.size) {
-                if (prices[i] < LOWER_BOUNDARY || prices[i] > UPPER_BOUNDARY) return Result.failure(
+                if (prices[i] < PRICES_COST_LOWER_BOUNDARY || prices[i] > PRICES_COST_UPPER_BOUNDARY) return Result.failure(
                     OutOfBoundException(
                         "Prices must be greater than or equal to 0 and less than or equal to 1000"
                     )
--- src/test/kotlin/com/hits/testing/alg/MaxProfitTest.kt
+++ src/test/kotlin/com/hits/testing/alg/MaxProfitTest.kt
@@ -2,6 +2,12 @@ package com.hits.testing.alg
 
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
 
 class MaxProfitTest {
     @Nested
@@ -35,6 +41,49 @@ class MaxProfitTest {
             val prices = IntArray(10001) { it }
             val result = MaxProfit.calculate(prices)
             assert(result.isFailure)
+        }
+    }
+
+    @Nested
+    inner class CommonWorkflow {
+        @ParameterizedTest
+        @ArgumentsSource(CommonArgumentsProvider::class)
+        fun `return expected result`(pricesAndResult: Pair<IntArray, Int>) {
+            val result = MaxProfit.calculate(pricesAndResult.first)
+            assert(result.isSuccess)
+            result.getOrNull()?.let {
+                assert(it == pricesAndResult.second)
+            }
+        }
+
+        @ParameterizedTest
+        @ArgumentsSource(CrossOverArgumentsProvider::class)
+        fun `return 0 if profit cannot be achieved`(prices: IntArray) {
+            val result = MaxProfit.calculate(prices)
+            assert(result.isSuccess)
+            result.getOrNull()?.let {
+                assert(it == 0)
+            }
+        }
+    }
+
+    private class CommonArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
+            return Stream.of(
+                Arguments.of(Pair(intArrayOf(1), 0)),
+                Arguments.of(Pair(intArrayOf(1, 2, 3, 4), 3)),
+                Arguments.of(Pair(intArrayOf(1, 23, 234, 999), 998)),
+                Arguments.of(Pair(intArrayOf(7, 0, 1, 4, 9, 10, 54, 100), 100)),
+            )
+        }
+    }
+
+    private class CrossOverArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
+            return Stream.of(
+                Arguments.of(intArrayOf(5, 4, 3, 2, 1)),
+                Arguments.of(intArrayOf(1023, 512, 312, 74, 73, 72, 21, 20, 20, 20, 20, 10))
+            )
         }
     }
 }
