6675c0405d4b0f11ee1f38f2991e3686c065ad87
vladovello <vladsuhankov@gmail.com>
2023-05-01 16:52:58 +0700

    :recycle: refactor(Alg): Changed code, changed return type to Result

--- src/main/kotlin/com/hits/testing/alg/MaxProfit.kt
+++ src/main/kotlin/com/hits/testing/alg/MaxProfit.kt
@@ -1,18 +1,17 @@
 package com.hits.testing.alg
 
-class MaxProfit {
-    operator fun invoke(prices: IntArray): Int {
-        var buy = prices[0]
-        var maxDiff = 0
-        for (i in 1 until prices.size) {
-            buy = buy.coerceAtMost(prices[i])
-            maxDiff = maxDiff.coerceAtLeast(prices[i] - buy)
-        }
-        return maxDiff
-    }
+class MaxProfit private constructor() {
+    companion object {
+        fun calculate(prices: IntArray): Result<Int> {
+            var buy = prices[0]
+            var maxDiff = 0
 
-}
-fun main() {
-    val arr = arrayOf<Int>()
-    println(arr[0])
+            for (i in 1 until prices.size) {
+                buy = buy.coerceAtMost(prices[i])
+                maxDiff = maxDiff.coerceAtLeast(prices[i] - buy)
+            }
+
+            return Result.success(maxDiff)
+        }
+    }
 }
