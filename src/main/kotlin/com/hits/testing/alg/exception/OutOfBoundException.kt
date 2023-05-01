9035dc0b28851d5d89aa5535351df452f44c4e6a
vladovello <vladsuhankov@gmail.com>
2023-05-01 17:40:52 +0700

    :bug: fix(Alg-unit): Added check for prices array length

--- src/main/kotlin/com/hits/testing/alg/exception/OutOfBoundException.kt
+++ src/main/kotlin/com/hits/testing/alg/exception/OutOfBoundException.kt
@@ -1,4 +1,3 @@
 package com.hits.testing.alg.exception
 
-open class OutOfBoundException :
-    InvalidDataException("Prices must be greater than or equal to 0 and less than or equal to 1000")
+open class OutOfBoundException(override val message: String? = null) : InvalidDataException(message)
